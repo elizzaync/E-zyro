b53a2601fb5e04a82ab8b87e67046650a328db63
Gumball0506 <HaroldOrtiz@outlook.es>
2026-04-20 15:11:20 -0500

    pruebas

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Usuario(BaseModel):
-    nombre: str
-    edad: int
-
 @app.get("/")
 def read_root():
-    return {"mensaje": "API funcionando 🚀"}
-
-@app.post("/usuarios")
-def crear_usuario(usuario: Usuario):
-    return {
-        "mensaje": "Usuario creado",
-        "data": usuario
-    }
+    return {"mensaje": "Hola Mundo desde Railway! 🚀"}
