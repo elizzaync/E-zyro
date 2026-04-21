f5ca10ff412383337d0ce61dd4a1b58227c13709
Gumball0506 <HaroldOrtiz@outlook.es>
2026-04-21 11:45:37 -0500

    cors incorporados

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -1,18 +1,22 @@
-# BACKEND/app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth
-app = FastAPI(title="API E-zyro Backend")
-# Configuración de CORS
+app = FastAPI(title="API E-zyro")
+# 1. Configurar los dominios permitidos (CORS)
+origenes_permitidos = [
+    "http://localhost:4200",  # Tu Angular en local
+    # Aquí luego agregaremos la URL de tu frontend en producción
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Cambiar en producción
+    allow_origins=origenes_permitidos,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"], # Permite POST, GET, PUT, DELETE
+    allow_headers=["*"], # Permite enviar tokens y headers
 )
-# Conectamos el router de autenticación a la app principal
+# 2. Incluir los routers
 app.include_router(auth.router)
+
 @app.get("/")
 def read_root():
-    return {"mensaje": "API de e-zyro funcionando en Railway 🚀"}
+    return {"mensaje": "Backend de E-zyro activo"}
