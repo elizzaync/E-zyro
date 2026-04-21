d3567e1ab6a4b505ab9c5b9cae6948416e0ed162
Gumball0506 <HaroldOrtiz@outlook.es>
2026-04-21 15:15:28 -0500

    fix: Agregar schema PasswordResetRequest

--- BACKEND/app/schemas/auth.py
+++ BACKEND/app/schemas/auth.py
@@ -1,4 +1,8 @@
 from pydantic import BaseModel
+
+# Tu plantilla actual para el Login
 class LoginData(BaseModel):
     username: str
     password: str
+class PasswordResetRequest(BaseModel):
+    email: str
