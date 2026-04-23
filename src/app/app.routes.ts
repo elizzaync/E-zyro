1ac9cfcbc7727732de20bacedf2a8c4a21e28121
Gumball0506 <HaroldOrtiz@outlook.es>
2026-04-23 11:11:21 -0500

    mejoras

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
-
 // --- NUESTRO NUEVO COMPONENTE UNIFICADO ---
 import { ResetPasswordComponent } from './features/auth/reset-password/reset-password.component';
-
 // --- IMPORTAMOS EL DASHBOARD Y EL GUARDIÁN ---
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { authGuard } from './core/guards/auth.guards';
