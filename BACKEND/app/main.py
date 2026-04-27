9cf3bc1fe6c517d5cf150acdfa199b6ce01169f0
Gumball0506 <HaroldOrtiz@outlook.es>
2026-04-27 15:42:08 -0500

    cambios en main

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -1,37 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-# IMPORTACIONES LOCALES (Sin el "app.")
-from database import engine, Base
-from models.empresa import Empresa
-from models.usuario import Usuario
-from models.empleado import Empleado
-from models.orden_mantenimiento import OrdenMantenimiento
-from models.notificacion import Notificacion
-
-from routers import auth, dashboard
-
-# Crea las tablas
-Base.metadata.create_all(bind=engine)
-
+from app.routers import auth
 app = FastAPI(title="API E-zyro")
-
+# 1. Configurar los dominios permitidos (CORS)
 origenes_permitidos = [
     "http://localhost:4200",
-    "*"
+      "*"  # Tu Angular en local
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origenes_permitidos,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"], # Permite POST, GET, PUT, DELETE
+    allow_headers=["*"], # Permite enviar tokens y headers
 )
-
+# 2. Incluir los routers
 app.include_router(auth.router)
-app.include_router(dashboard.router)
-
 @app.get("/")
 def read_root():
-    return {"mensaje": "Backend de E-zyro activo y modularizado"}
+    return {"mensaje": "Backend de E-zyro activo"}
