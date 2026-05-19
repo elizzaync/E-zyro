9e631a456a50bd786d1df0693036d7243452a4b5
Gumball0506 <haroldortiz@outlook.es>
2026-05-19 15:38:25 -0500

    problema

--- BACKEND/app/models/rol_permiso.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, ForeignKey
-from app.db.database import Base
-
-class RolPermiso(Base):
-    __tablename__ = "rol_permiso"
-
-    rol_id = Column(String(36), ForeignKey("rol.id"), primary_key=True)
-    permiso_id = Column(String(36), ForeignKey("permiso.id"), primary_key=True)
