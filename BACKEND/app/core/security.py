d155cbfdc97413d4789a41bf43c052acc119fed0
E-System TIC <desarrollo.esystemtic@gmail.com>
2026-05-19 16:44:56 -0500

    backenddd harold pvto

--- BACKEND/app/core/security.py
+++ BACKEND/app/core/security.py
@@ -7,7 +7,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 1440  # 24 horas
+ACCESS_TOKEN_EXPIRE_MINUTES = 10080  # 7 días
 security = HTTPBearer()
 def crear_token_acceso(data: dict):
     to_encode = data.copy()
