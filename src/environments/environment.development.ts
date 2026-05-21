f606ef05a180e7c4f45d01db63c6c81bd1dcb335
Gumball0506 <haroldortiz@outlook.es>
2026-05-21 09:48:18 -0500

    Merge branch 'frontend' of https://github.com/elizzaync/E-zyro into frontend

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://api.mystic-byte.com/',
+  firebaseConfig: {
+    apiKey: "AIzaSyApBrdBJkIRUoqEkMJCiEuVg4saZ_y0iVE",
+    authDomain: "e-system-tic.firebaseapp.com",
+    databaseURL: "https://e-system-tic-default-rtdb.firebaseio.com",
+    projectId: "e-system-tic",
+    storageBucket: "e-system-tic.firebasestorage.app",
+    messagingSenderId: "603001313879",
+    appId: "1:603001313879:web:295d77a879b7020d2c2112",
+    measurementId: "G-Z9TMBPG38Q"
+  },
+  vapidKey: "BLbX7UlEoor20chdYc95BOhpSSeuDPCjD8ysLjtRcdGXKDvh-WCMXQFWvzp4wGmS3yKJQGffudaUgZgNPlyc7W4"
+};
